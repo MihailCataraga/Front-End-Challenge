4e312be149c1624c761eb1ff544de9923adbf9b5
Mihai <cataraga.95@gmail.com>
2023-03-11 15:43:45 +0200

    5 task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useState } from 'react';
 import Keyboard from './components/Keyboard';
 
 function App() {
+  var [arr, setArr] = useState('');
+  var [i, setI] = useState(0)
+
   // onkeydown
   window.onkeydown = function(e) {
     if(e.code === document.getElementById(e.code).innerHTML) {
       document.getElementById("box-" + e.code).classList.add('blue')
+      setArr(arr => [...arr, e.code])
     }
     setTimeout(() => {
       document.getElementById("box-" + e.code).classList.remove("blue");
@@ -14,9 +19,40 @@ function App() {
       document.getElementById("box-" + e.code).classList.remove("green");
     }, 60000)
   };
+
+  // Repeat btn
+  const repeat = () => {
+    setI(i = 0);
+    setInterval(() => {
+      if(i < arr.length) {
+          document.getElementById("box-" + arr[i]).classList.remove("blue");
+          document.getElementById("box-" + arr[i]).classList.remove("green");
+          document.getElementById("box-" + arr[i]).classList.add('blue');
+          setI(i++)
+      }
+    }, 200);
+    for(let i = 0; i < arr.length; i++) {
+      document.getElementById("box-" + arr[i]).classList.remove("blue");
+      document.getElementById("box-" + arr[i]).classList.remove("green");
+    }
+  }
+
+  // Reset History btn
+  const reset = () => {
+    for(let i = 0; i < arr.length; i++) {
+      document.getElementById("box-" + arr[i]).classList.remove("blue");
+      document.getElementById("box-" + arr[i]).classList.remove("green");
+    };
+    setArr('');
+  }
   return (
     <div className="App d-flex a-center">
+      <textarea />
       <Keyboard />
+      <div className='btns d-flex'>
+        <button onClick={repeat}>Repeat</button>
+        <button onClick={reset}>Reset History</button>        
+      </div>
     </div>
   );
 }
