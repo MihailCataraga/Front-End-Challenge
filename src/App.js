c9b6538bfca4a3bd63862a3f1494965a3a258481
Mihai <cataraga.95@gmail.com>
2023-03-11 15:22:23 +0200

    2 task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import Keyboard from './components/Keyboard';
 
 function App() {
+  // onkeydown event
+  window.onkeydown = function(e) {
+    if(e.code === document.getElementById(e.code).innerHTML) {
+      document.getElementById("box-" + e.code).classList.add('blue')
+    }
+  };
+  // onkeyup event
+  window.onkeyup = function(e) {
+    if(e.code === document.getElementById(e.code).innerHTML) {
+      document.getElementById("box-" + e.code).classList.remove("blue");
+      document.getElementById("box-" + e.code).classList.add('green');
+    }
+  }
   return (
     <div className="App">
       <Keyboard />
--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 export default function Key(props) {
   return (
     <div
-        className={props.class + ' d-flex a-center j-center'}>
+        className={props.class + ' d-flex a-center j-center '}
+        id={"box-" + props.code}
+    >
         <h4>{props.secName}</h4>
         <h4>{props.name}</h4>
+        <p id={props.code}>{props.code}</p>
     </div>
   )
 }
-
